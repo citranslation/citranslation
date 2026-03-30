5814f528ff6cfd81644a09133a44a06a70804935
wu <wu@.com>
2026-03-30 13:29:14 +0800

    update

--- scripts/run_CheckBuildResult.py
+++ scripts/run_CheckBuildResult.py
@@ -6,12 +6,17 @@ from citranslation.core.Iterative import run
 
 
 if __name__ == "__main__":
+    """
+    test repo dir: personal repo
+    model tag: gemini3 or gpt-4o or gpt-40-mini or deepseek or importer
+
+    """
     base_dir = Path(__file__).resolve().parent.parent
-    # test repo dir
+
     test_dir = 'XXXX'
-    csv_path = base_dir/"citranslation"/"resources"/"csv"/'datasets.csv'
-    # model tag: gemini3 or gpt-4o or gpt-40-mini or deepseek or importer
     model_tag = 'gemini3'
+    csv_path = base_dir/"citranslation"/"resources"/"csv"/'datasets.csv'
+
     df = pd.read_csv(csv_path)
     for index, row in df.iterrows():
         repo_name = row['repo_name']
--- scripts/run_GenCItraFiles.py
+++ scripts/run_GenCItraFiles.py
@@ -5,13 +5,17 @@ from citranslation.core.GenCItraFiles import run
 
 
 if __name__ == "__main__":
+    """
+    dir_name: save file in 'translation' or 'enhancement'
+    prompt_type: prompt type 'base' or 'oneshot' or 'guideline'
+    model_tag: gen ci file from 'gemini3' or 'gpt-4o' or 'gpt-40-mini'
+    """
+
     base_dir = Path(__file__).resolve().parent.parent
-    # csv_path: dataset csv path
-    csv_path = base_dir/"citranslation"/"resources"/"csv"/'datasets.csv'
-    # dir_name: save file in 'translation' or 'enhancement'
+
     dir_name = 'enhancement'
-    # prompt_type: prompt type 'base' or 'oneshot' or 'guideline'
     prompt_type = 'guideline'
-    # model_tag: gen ci file from 'gemini3' or 'gpt-4o' or 'gpt-40-mini'
     model_tag = 'gemini3'
+    csv_path = base_dir/"citranslation"/"resources"/"csv"/'datasets.csv'
+
     run(csv_path, dir_name, prompt_type, model_tag)
